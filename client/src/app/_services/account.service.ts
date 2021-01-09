092f248fcd3d896ca78243349d86fe3e0380aabb
Elli Rezagholi <e.rezagholi@gmail.com>
2021-01-09 21:00:04 +0330

    End of session 5

---
+++ client/src/app/_services/account.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+import { User } from '../_models/user';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AccountService {
+
+  baseUrl = 'https://localhost:5001/api/';
+  private currenUserSource = new ReplaySubject<User>(1);
+  currentUser$ = this.currenUserSource.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(model: any) {
+    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+      map((response: any) => {
+        const user = response;
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currenUserSource.next(user);
+        }
+      })
+    )
+  }
+
+  register(model: any){
+    return this.http.post(this.baseUrl+'account/register',model).pipe(
+      map((user: any)=>{
+        if(user){
+          localStorage.setItem('user',JSON.stringify(user));
+          this.currenUserSource.next(user);
+        }
+        return user;
+      })  
+    )
+  }
+
+  setCurrentUser(user: User) {
+    this.currenUserSource.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currenUserSource.next();
+  }
+}
+
+
--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,26 +1,31 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
   title = 'The Dating App';
-  users:any;
+  users: any;
 
-  constructor(private http:HttpClient){}
+  constructor(private accountService: AccountService) { }
 
-  ngOnInit(){
-    this.GetUsers();
+  ngOnInit() {
+    this.setCurrentUser();
   }
 
-  GetUsers(){
-    this.http.get('https://localhost:5001/api/users').subscribe(Response=> {
-      this.users=Response;
-    } , error=> {
-      console.log(error);
-    })
+  setCurrentUser() {
+    //As the error says, localStorage.getItem() can return either a string or null.JSON.parse() requires a string, so you should test the result of localStorage.getItem() before you try to use it.
+    //For example:
+    //const user: User = JSON.parse(localStorage.getItem('user') || '{}');
+    //or perhaps:
+    //const userJson = localStorage.getItem('user');
+    //const user = userJson !== null ? JSON.parse(userJson) : new User();
+    //See also the answer from Willem De Nys. If you are confident that the localStorage.getItem() call can never return null you can use the non-null assertion operator to tell typescript that you know what you are doing:
+    const user: User = JSON.parse(localStorage.getItem('user')!);
+    this.accountService.setCurrentUser(user);
   }
 }
